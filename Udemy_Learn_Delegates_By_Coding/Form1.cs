5f00aca5655347fd5f90689a447512a0e0c2f50d
Sean Newman <seangnewman@msn.com>
2018-09-23 17:54:38 -0700

    Add project files.

---
+++ Udemy_Learn_Delegates_By_Coding/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Udemy_Learn_Delegates_By_Coding
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
